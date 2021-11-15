10e941881a3211aa109494bc09e1100f79bdc749
Artem Maksymov <armaksimov3@gmail.com>
2021-11-14 21:48:27 -0500

    Triangle functional expanded in main.c and triangleSolver.c modified.
    Small alterations:
    1. All static arrays are changed to dynamic.
    2. triangleSolver.c function analyzeTriangle() now accepts a dynamic array.
    3. triangleSolver.c function analyzeTriangle() now checks if the triangle can be created.
    
    Functions added in main.c:
    1. calculateInsideAngles(double *sides) - function accepts a double array and then calculates the inside angles of a triangle.
    2. radToDegree function in main.c added and is created to convert radians to degrees.
    3. printAngles function  added in main.c to enable printing of inside angles of a triangle.

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -1,5 +1,7 @@
-	#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "main.h"
 #include "triangleSolver.h"
@@ -8,32 +10,45 @@
 #define NUM_OF_TRIANGLE_SIDES 3
 #define NUM_OF_RECTANGLE_SIDES 4
 #define SUM_OF_ANGLES_IN_TRIANGLE 180
+#define PI 3.14159265359
 
 void printWelcome();
 int printShapeMenu();
 void getTriangleSides(double* triangleSides);
 double scanCheckSide(int i);
+double* calculateInsideAngles(double* sides);
+double radToDegree(double rad);
+void printAngles(double* angle);
 
 int main() {
 	while(1) {
 		printWelcome();
 
 		int shapeChoice = printShapeMenu();
+		double* triangleSidesPtr = (double*) calloc(NUM_OF_TRIANGLE_SIDES, sizeof(double));
 
 		switch (shapeChoice) {
 			case 1:
 				printf_s("Triangle selected.\n");
-				double triangleSides[NUM_OF_TRIANGLE_SIDES] = { 0, 0, 0 };
-				double* triangleSidesPtr = &triangleSides;
+
 				getTriangleSides(triangleSidesPtr);
+				char* result = analyzeTriangle(triangleSidesPtr);
 
+				if (strcmp(result, "Not a triangle") == 0) {
+					printf_s("%s\n", result);
+					break;
+				}
+				else {
+					double* angles = calculateInsideAngles(triangleSidesPtr);
 
-				char* result = analyzeTriangle(triangleSidesPtr[0], triangleSidesPtr[1], triangleSidesPtr[2]);
-				printf_s("%s\n", result);
+					printAngles(angles);
+					printf_s("%s\n", result);
+				}
 				break;
 			case 0: return 0;
 			default: printf_s("Invalid value entered.\n"); break;
 		}
+		free(triangleSidesPtr);
 	}
 	return 0;
 }
@@ -80,4 +95,29 @@ double scanCheckSide(int i) {
 		else correctnessIndicator = true;
 	} while (correctnessIndicator == false);
 	return side;
+}
+
+double* calculateInsideAngles(double* sides) {
+	double* resultPTR = (double*)calloc(NUM_OF_TRIANGLE_SIDES, sizeof(double));
+
+	double pow0 = pow(*(sides + 0), 2);
+	double pow1 = pow(*(sides + 1), 2);
+	double pow2 = pow(*(sides + 2), 2);
+	*(resultPTR + 0) = radToDegree(acos((pow1 + pow2 - pow0) / (2 * *(sides + 1) * *(sides + 2))));
+	*(resultPTR + 1) = radToDegree(acos((pow0 + pow1 - pow2) / (2 * *(sides + 0) * *(sides + 1))));
+	*(resultPTR + 2) = SUM_OF_ANGLES_IN_TRIANGLE - *(resultPTR + 0) - *(resultPTR + 1);
+	return resultPTR;
+}
+
+double radToDegree(double rad) {
+	double degree;
+	degree = (rad * 180) / PI;
+	return degree;
+}
+
+void printAngles(double* angle) {
+	int i;
+	for (i = 0; i < NUM_OF_TRIANGLE_SIDES; i++) {
+		printf("Angle #%d = %f\n", i, *(angle + i));
+	}
 }
--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -3,3 +3,6 @@ void printWelcome();
 int printShapeMenu();
 void getTriangleSides(double* triangleSides);
 double scanCheckSide(int i);
+double* calculateInsideAngles(double* sides);
+double radToDegree(double rad);
+void printAngles(double* angle);
