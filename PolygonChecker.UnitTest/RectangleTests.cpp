d571e4dec218244d4e44d99ad52b16d5c3191453
William Kubica <Kirbycopy56@hotmail.com>
2021-11-18 22:20:28 -0500

    added more rectangles unit tests

--- PolygonChecker.UnitTest/RectangleTests.cpp
+++ PolygonChecker.UnitTest/RectangleTests.cpp
@@ -20,11 +20,50 @@ namespace RectanlgeTests
 			bool a = ifRightAngles(0, 0, true);
 			Assert::AreEqual(a, true);
 		}
-		TEST_METHOD(test2) {
-			double points[8] = { 2,2,-2,2,-2,-2,2,-2 };
+		TEST_METHOD(RectSolver_Square1) {
+			double points[8] = { 2,2,   -2,2,   -2,-2,   2,-2 };
 			char* a = analyzeRect(points);
 			int b = strncmp(a, "Points DO form a rectangle.", 60);
 			Assert::AreEqual(b, 0);
+		}
+		TEST_METHOD(RectSolver_Square2) {
+			double points[8] = { 2,2 ,-2,-2 ,-2,2 ,2,-2 };
+			char* a = analyzeRect(points);
+			int b = strncmp(a, "Points DO form a rectangle.", 60);
+			Assert::AreEqual(b, 0);
+		}
+		TEST_METHOD(RectSolver_Square3) {
+			double points[8] = { 2,1 ,1,2 ,2,3 ,3,2 };
+			char* a = analyzeRect(points);
+			int b = strncmp(a, "Points DO form a rectangle.", 60);
+			Assert::AreEqual(b, 0);
+		}
+		TEST_METHOD(RectSolver_Square4) {
+			double points[8] = { 2,3 ,3,2 ,1,2 ,2,1 };
+			char* a = analyzeRect(points);
+			int b = strncmp(a, "Points DO form a rectangle.", 60);
+			Assert::AreEqual(b, 0);
+		}
+		TEST_METHOD(RectSolver_Slope1) {
+			double a = calculateSlope(1,1,2,2,false);
+			Assert::AreEqual(a, 1.0);
+		}
+		TEST_METHOD(RectSolver_Slope2) {
+			double a = calculateSlope(1, 1, 4, 2, false);
+			Assert::AreEqual(a, 1.0 / 3.0);
+		}
+		TEST_METHOD(RectSolver_Slope3) {
+			double a = calculateSlope(1, 1, 2, 4, false);
+			Assert::AreEqual(a, 3.0);
+		}
+		TEST_METHOD(RectSolver_RightAngle1) {
+			Assert::IsTrue(ifRightAngles(1, -1, false));
+		}
+		TEST_METHOD(RectSolver_RightAngle2) {
+			Assert::IsTrue(ifRightAngles(3, -1/3, false));
+		}
+		TEST_METHOD(RectSolver_RightAngle3) {
+			Assert::IsTrue(ifRightAngles(0, -1, true));
 		}
 	};
 }
