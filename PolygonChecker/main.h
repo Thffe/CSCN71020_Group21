c6295320ed90bb54be1e56020d0648fd30fe9862
William Kubica <Kirbycopy56@hotmail.com>
2021-11-21 20:45:42 -0500

    moved rect area function into rectSolver.c. modified and added comments to rectSolver. slight adjustment to main.c

--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -2,7 +2,6 @@
 double* getRectPoints(double* points);
 double calculatePerimeter(double* points);
 double calculateDist(double x1, double y1, double x2, double y2);
-double calculateArea(double* rectanglePTS);
 
 void getTriangleSides(double* triangleSides);
 double scanCheckSide(int i);
--- PolygonChecker/rectSolver.h
+++ PolygonChecker/rectSolver.h
@@ -2,3 +2,4 @@
 char* analyzeRect(double* points);
 double calculateSlope(double x1, double y1, double x2, double y2, bool vert);
 bool ifRightAngles(double s1, double s2, bool vert);
+double calculateArea(double side1, double side2);
