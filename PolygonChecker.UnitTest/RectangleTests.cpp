9a0ab54ec299cecfed3ec5dfdedfd559e0860711
William Kubica <Kirbycopy56@hotmail.com>
2021-11-21 22:37:54 -0500

    added more rectangle tests

--- PolygonChecker.UnitTest/RectangleTests.cpp
+++ PolygonChecker.UnitTest/RectangleTests.cpp
@@ -5,7 +5,8 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 extern "C"
 {
-#include "rectSolver.h"		// include any other .h files you would want to.
+#include "rectSolver.h"	
+#include "main.h"		// include any other .h files you would want to.
 }
 
 namespace RectanlgeTests
@@ -14,32 +15,62 @@ namespace RectanlgeTests
 	{
 	public:
 
-		TEST_METHOD(TestMethod1)
-		{
-			//double points[8] = {};
-			bool a = ifRightAngles(0, 0, true);
-			Assert::AreEqual(a, true);
-		}
-		TEST_METHOD(RectSolver_Square1) {
+		TEST_METHOD(RectSolver_Square1_1) {
 			double points[8] = { 2,2,   -2,2,   -2,-2,   2,-2 };
 			char* a = analyzeRect(points);
 			int b = strncmp(a, "Points DO form a rectangle.", 60);
 			Assert::AreEqual(b, 0);
 		}
-		TEST_METHOD(RectSolver_Square2) {
+		TEST_METHOD(RectSolver_Square1_2) {
 			double points[8] = { 2,2 ,-2,-2 ,-2,2 ,2,-2 };
 			char* a = analyzeRect(points);
 			int b = strncmp(a, "Points DO form a rectangle.", 60);
 			Assert::AreEqual(b, 0);
 		}
-		TEST_METHOD(RectSolver_Square3) {
+		TEST_METHOD(RectSolver_Square1_3) {
+			double points[8] = { -2,-2 ,2,2 ,2,-2 ,-2,2 };
+			char* a = analyzeRect(points);
+			int b = strncmp(a, "Points DO form a rectangle.", 60);
+			Assert::AreEqual(b, 0);
+		}
+		TEST_METHOD(RectSolver_Square1_4) {
+			double points[8] = { 2,-2 ,-2,-2 ,2,2 ,-2,2 };
+			char* a = analyzeRect(points);
+			int b = strncmp(a, "Points DO form a rectangle.", 60);
+			Assert::AreEqual(b, 0);
+		}
+		TEST_METHOD(RectSolver_Square2_1) {
 			double points[8] = { 2,1 ,1,2 ,2,3 ,3,2 };
 			char* a = analyzeRect(points);
 			int b = strncmp(a, "Points DO form a rectangle.", 60);
 			Assert::AreEqual(b, 0);
 		}
-		TEST_METHOD(RectSolver_Square4) {
+		TEST_METHOD(RectSolver_Square2_2) {
 			double points[8] = { 2,3 ,3,2 ,1,2 ,2,1 };
+			char* a = analyzeRect(points);
+			int b = strncmp(a, "Points DO form a rectangle.", 60);
+			Assert::AreEqual(b, 0);
+		}
+		TEST_METHOD(RectSolver_Square2_3) {
+			double points[8] = { 1,2 ,2,3 ,2,1 ,3,2 };
+			char* a = analyzeRect(points);
+			int b = strncmp(a, "Points DO form a rectangle.", 60);
+			Assert::AreEqual(b, 0);
+		}
+		TEST_METHOD(RectSolver_Rect1_1) {
+			double points[8] = { 2,2 ,2,-2 ,-5,2 ,-5,-2 };
+			char* a = analyzeRect(points);
+			int b = strncmp(a, "Points DO form a rectangle.", 60);
+			Assert::AreEqual(b, 0);
+		}
+		TEST_METHOD(RectSolver_Rect1_2) {
+			double points[8] = { 3,0 ,0,3 ,-2,-5 ,-5,-2 };
+			char* a = analyzeRect(points);
+			int b = strncmp(a, "Points DO form a rectangle.", 60);
+			Assert::AreEqual(b, 0);
+		}
+		TEST_METHOD(RectSolver_Rect2_1) {
+			double points[8] = { 0.3,2.1 ,-2.4,1.2 ,-0.6,-4.2 ,2.1,-3.3 };
 			char* a = analyzeRect(points);
 			int b = strncmp(a, "Points DO form a rectangle.", 60);
 			Assert::AreEqual(b, 0);
@@ -64,6 +95,22 @@ namespace RectanlgeTests
 		}
 		TEST_METHOD(RectSolver_RightAngle3) {
 			Assert::IsTrue(ifRightAngles(0, -1, true));
+		}
+		TEST_METHOD(RectSolver_Area) {
+			double area = calculateArea(3.0, 3.0);
+			Assert::AreEqual(area, 9.0);
+		}
+		TEST_METHOD(RectSolver_Distance1) {
+			double dist = calculateDist(2,2 ,-2,2);
+			Assert::AreEqual(dist, 4.0);
+		}
+		TEST_METHOD(RectSolver_Distance2) {
+			double dist = calculateDist(-5,-5 ,5,5);
+			Assert::AreEqual(dist, sqrt(200));
+		}
+		TEST_METHOD(RectSolver_Distance3) {
+			double dist = calculateDist(2, -6, -3, 5);
+			Assert::AreEqual(dist, sqrt(146));
 		}
 	};
 }
